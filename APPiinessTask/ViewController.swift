d73e21c8972b2a1a450b1be347409b37646e4331
Santhosh_GitLab <santhosh.m@agnosticsolutions.in>
2020-09-23 05:27:28 +0530

    Initial Commit

---
+++ APPiinessTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  APPiinessTask
+//
+//  Created by admin on 9/23/20.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
