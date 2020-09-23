c52d3cfde43dc2e3b413902575dc6f470e23d30b
Santhosh_GitLab <santhosh.m@agnosticsolutions.in>
2020-09-23 07:54:41 +0530

    unitTested code

--- APPiinessTaskTests/APPiinessTaskTests.swift
+++ APPiinessTaskTests/APPiinessTaskTests.swift
@@ -10,9 +10,12 @@ import XCTest
 @testable import APPiinessTask
 
 class APPiinessTaskTests: XCTestCase {
-
+    var viewCtrl : ViewController!
+    var viewModel : ViewModel!
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        self.viewCtrl = ViewController()
+        self.viewModel = ViewModel()
     }
 
     override func tearDownWithError() throws {
@@ -20,8 +23,63 @@ class APPiinessTaskTests: XCTestCase {
     }
 
     func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssertNotNil(self.viewCtrl)
+        XCTAssertNotNil(self.viewModel)
+        XCTAssertNil(self.viewCtrl.tableView)
+    }
+    func testStudentRecodeTest () throws{
+        if self.viewCtrl.studentRecord.count > 0 {
+             XCTAssertNotNil(self.viewCtrl.studentRecord)
+            XCTAssertTrue(self.viewCtrl.studentRecord.count > 0 )
+        }else{
+            XCTAssertNotNil(self.viewCtrl.studentRecord)
+            XCTAssertFalse(self.viewCtrl.studentRecord.count > 0 )
+        }
+    }
+    func testCodegetRecoder() throws {
+        if self.viewModel.getStudentInfo().count > 0 {
+              XCTAssertTrue(self.viewModel.getStudentInfo().count > 0 )
+        }else{
+            XCTAssertFalse(self.viewModel.getStudentInfo().count > 0 )
+        }
+    }
+    
+    func testViewModelStudentInfo() throws {
+        XCTAssertNotNil(self.viewModel.studInfo)
+    }
+    
+    //MARK:- code tested for allStudent DoB not a leap Year
+    func testAllyearnotLeapYear() throws{
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat  = "yyyy/MM/dd"
+        
+          if self.viewModel.getStudentInfo().count > 0 {
+            
+            for stud in self.viewModel.getStudentInfo() {
+                let dobStr = dateFormatter.string(from: stud.dob)
+                print("dobStr : \(dobStr)")
+                let dateArr : [String] = dobStr.components(separatedBy: "/")
+                guard let year = Int(dateArr[0]) else{
+                    return
+                }
+                
+                XCTAssertTrue((year % 4) != 0)
+            }
+            
+        }
+    }
+    
+    //MARK:- tested  code based on studen DOB ascending order 
+    func testDobOrderStudent() throws {
+        let studInfo = self.viewModel.getStudentInfo()
+        
+        if studInfo.count > 0 {
+            for i in 0..<studInfo.count - 1 {
+                if i != studInfo.count  {
+                    XCTAssertTrue(studInfo[i].dob < studInfo[i+1].dob )
+                }
+            }
+        }
     }
 
     func testPerformanceExample() throws {
